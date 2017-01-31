27c85f1b747d5f293c3c5444f0256fe93cdcfb21
Chris Sang <Chris@Chriss-MacBook-Pro.local>
2017-01-31 00:08:33 -0500

    updated php

--- form.php
+++ form.php
@@ -1,63 +1,11 @@
 <?php
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
+if ($_POST['submit']) {
+    mail("kunal@eml.cc", "Form Submission",
+        "Name: ".$_POST['first_name']."
+        LastName: ".$_POST['last_name']."
+        Email: ".$_POST['email']."
+        Phone: ".$_POST['phone']."
+        City: ".$_POST['city']."
+        LearningGoals: ".$_POST['comment']);
 }
-$name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$visitor_email = $_POST['email'];
-$phone = $_POST['phone'];
-$city = $_POST['city'];
-$message = $_POST['comment'];
-
-//Validate first
-if(empty($name)||empty($visitor_email))
-{
-    echo "Name and email are mandatory!";
-    exit;
-}
-
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
-
-$email_subject = "Form submission";
-$email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".
-
-$to = "kunal@eml.cc";//<== update the email address
-$headers = "From: $visitor_email \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-//Send the email!
-mail($to,$email_subject,$email_body,$headers);
-//done. redirect to thank-you page.
-header('Location: thank-you.html');
-
-
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
-    return true;
-  }
-  else
-    {
-    return false;
-  }
-}
-
 ?>
